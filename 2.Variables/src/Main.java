571bc0fec21f07a208bb9e2047cebcbf092440a7
scorpion <heshamscorpe55@gmail.com>
2015-08-01 15:31:52 +0200

    Add 2.Varaibles

---
+++ 2.Variables/src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+	public static void main(String[] args) {
+		int integer      = Integer.SIZE;
+		int longInteger  = Long.SIZE;
+		int shortInteger = Short.SIZE;
+		int floatNumber  = Float.SIZE;
+		int doubleNumber = Double.SIZE;
+		int character    = Character.SIZE;
+		
+		System.out.printf("The size of int    : %d bit\n" , integer);
+		System.out.printf("The size of long   : %d bit\n" , longInteger);
+		System.out.printf("The size of short  : %d bit\n" , shortInteger);
+		System.out.printf("The size of float  : %d bit\n" , floatNumber);
+		System.out.printf("The size of double : %d bit\n" , doubleNumber);
+		System.out.printf("The size of char   : %d bit\n" , character);
+				
+	}
+}
