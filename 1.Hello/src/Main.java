3e7dbed3b9a25a73025232bede2e282c0ffac54e
scorpion <heshamscorpe55@gmail.com>
2015-08-01 13:01:46 +0200

    Add 1.Hello

---
+++ 1.Hello/src/Main.java
@@ -0,0 +1,7 @@
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Misr public library");
+		System.out.println("ãÑÍÈÇğ Èßã İí ãßÊÈÉ ãÕÑ ÇáÚÇãÉ ");
+	}
+}
